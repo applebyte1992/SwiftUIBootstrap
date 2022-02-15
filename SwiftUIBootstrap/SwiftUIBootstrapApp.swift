7100c7ef7d0dad9cd40730ab1f533175f285d806
Masroor Elahi <masroor.elahi@10pearls.com>
2022-02-15 17:35:35 +0500

    First commit

---
+++ SwiftUIBootstrap/SwiftUIBootstrapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootstrapApp.swift
+//  SwiftUIBootstrap
+//
+//  Created by Masroor Elahi on 15/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootstrapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIBootstrapUITests/SwiftUIBootstrapUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftUIBootstrapUITestsLaunchTests.swift
+//  SwiftUIBootstrapUITests
+//
+//  Created by Masroor Elahi on 15/02/2022.
+//
+
+import XCTest
+
+class SwiftUIBootstrapUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
